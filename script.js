4898f22cc04e8cb6fa390e845ef7a0ff258329a7
dreamer303 <nostalgicbimbo@gmail.com>
2025-08-28 00:25:45 +0600

    Call history added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//04. Clicking on the heart icon of any card will increase the count in the Navbar
 
 function heartCount(id) {
     document.getElementById(id).addEventListener('click', function () {
@@ -23,32 +22,16 @@ heartCount('favourite-btn-9');
 
 // Call Buttons
 
-// id1 - text, id2 - number , id3 = coin id
 
-/* function alertMsg(id1, id2) {
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById(id1).innerText
-    const callNum = document.getElementById(id2).innerText
-    const newCoin = oldCoin - 20 ;
-    Coin.innerText = newCoin;
-    if(newCoin>0){
-    alert("📞 calling " + text + " " + callNum + "...");
-    }
-    else{
-        alert("You don't have sufficient balance")
-    }
+const callHistory =[]
 
-} */
-
-// national emergency
-
-document.getElementById('call-btn-1').addEventListener('click', function () {
+function callAlertButton(callID,nameID, numID){
+    document.getElementById(callID).addEventListener('click', function () {
 
     const Coin = document.getElementById('coin-count')
     const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('ne').innerText
-    const callNum = document.getElementById('ne-num').innerText
+    const text = document.getElementById(nameID).innerText
+    const callNum = document.getElementById(numID).innerText
     const newCoin = oldCoin - 20;
     Coin.innerText = newCoin;
     if (newCoin <= 0) {
@@ -58,164 +41,41 @@ document.getElementById('call-btn-1').addEventListener('click', function () {
     else {
         alert("📞 calling " + text + " " + callNum + "...");
     }
-}
+
+    // div part
+     const storedData = {
+        name: text,
+        number: callNum,
+        date: new Date().toLocaleTimeString()
+    }
+    callHistory.push(storedData)
+
+    const getHistorySection = document.getElementById('history')
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div  class="flex justify-between items-center my-1.5 bg-[#f5fff6] lg:p-4">
+                <div >
+                    <p class="lg:text-2xl font-bold">${storedData.name}</p>
+                    <p class="lg:text-lg text-gray-600">${storedData.number}</p>
+                </div>
+                <p  class="lg:text-lg text-gray-600">${storedData.date}</p>
+
+            </div>
+        `
+        getHistorySection.appendChild(div)
+    }
 );
-
-// police helpline
-document.getElementById('call-btn-2').addEventListener('click', function () {
-
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('ph').innerText
-    const callNum = document.getElementById('ph-num').innerText
-    const newCoin = oldCoin - 20;
-    Coin.innerText = newCoin;
-    if (newCoin <= 0) {
-        alert("You don't have sufficient balance")
-
-    }
-    else {
-        alert("📞 calling " + text + " " + callNum + "...");
-    }
 }
-);
 
-// fire service
-
-document.getElementById('call-btn-3').addEventListener('click', function () {
-
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('fs').innerText
-    const callNum = document.getElementById('fs-num').innerText
-    const newCoin = oldCoin - 20;
-    Coin.innerText = newCoin;
-    if (newCoin <= 0) {
-        alert("You don't have sufficient balance")
-
-    }
-    else {
-        alert("📞 calling " + text + " " + callNum + "...");
-    }
-}
-);
-// Ambulance Service
-
-document.getElementById('call-btn-4').addEventListener('click', function () {
-
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('as').innerText
-    const callNum = document.getElementById('as-num').innerText
-    const newCoin = oldCoin - 20;
-    Coin.innerText = newCoin;
-    if (newCoin <= 0) {
-        alert("You don't have sufficient balance")
-
-    }
-    else {
-        alert("📞 calling " + text + " " + callNum + "...");
-    }
-}
-);
-
-// women and child
-document.getElementById('call-btn-5').addEventListener('click', function () {
-
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('wc').innerText
-    const callNum = document.getElementById('wc-num').innerText
-    const newCoin = oldCoin - 20;
-    Coin.innerText = newCoin;
-    if (newCoin <= 0) {
-        alert("You don't have sufficient balance")
-
-    }
-    else {
-        alert("📞 calling " + text + " " + callNum + "...");
-    }
-}
-);
-// Anti Corruption
-
-document.getElementById('call-btn-6').addEventListener('click', function () {
-
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('ac').innerText
-    const callNum = document.getElementById('ac-num').innerText
-    const newCoin = oldCoin - 20;
-    Coin.innerText = newCoin;
-    if (newCoin <= 0) {
-        alert("You don't have sufficient balance")
-
-    }
-    else {
-        alert("📞 calling " + text + " " + callNum + "...");
-    }
-}
-);
-// Electricity Helpline
-document.getElementById('call-btn-7').addEventListener('click', function () {
-
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('eh').innerText
-    const callNum = document.getElementById('eh-num').innerText
-    const newCoin = oldCoin - 20;
-    Coin.innerText = newCoin;
-    if (newCoin <= 0) {
-        alert("You don't have sufficient balance")
-
-    }
-    else {
-        alert("📞 calling " + text + " " + callNum + "...");
-    }
-}
-);
-// Brac Helpline
-
-document.getElementById('call-btn-8').addEventListener('click', function () {
-
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('bh').innerText
-    const callNum = document.getElementById('bh-num').innerText
-    const newCoin = oldCoin - 20;
-    Coin.innerText = newCoin;
-    if (newCoin <= 0) {
-        alert("You don't have sufficient balance")
-
-    }
-    else {
-        alert("📞 calling " + text + " " + callNum + "...");
-    }
-}
-);
-// railway Service 
-
-document.getElementById('call-btn-9').addEventListener('click', function () {
-
-    const Coin = document.getElementById('coin-count')
-    const oldCoin = parseInt(Coin.innerText)
-    const text = document.getElementById('rs').innerText
-    const callNum = document.getElementById('rs-num').innerText
-    const newCoin = oldCoin - 20;
-    Coin.innerText = newCoin;
-    if (newCoin <= 0) {
-        alert("You don't have sufficient balance")
-
-    }
-    else {
-        alert("📞 calling " + text + " " + callNum + "...");
-    }
-}
-);
+callAlertButton('call-btn-1','ne','ne-num');
+callAlertButton('call-btn-2','ph','ph-num');
+callAlertButton('call-btn-3','fs','fs-num');
+callAlertButton('call-btn-4','as','as-num');
+callAlertButton('call-btn-5','wc','wc-num');
+callAlertButton('call-btn-6','ac','ac-num');
+callAlertButton('call-btn-7','eh','eh-num');
+callAlertButton('call-btn-8','bh','bh-num');
+callAlertButton('call-btn-9','rs','rs-num');
 
 
-
-
-
-// const alertMsg1 =alertMsg('ne', 'ne-num');
 
